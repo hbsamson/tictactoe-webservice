a2d0366dc3f4aad6a508dcdaf7f277638bbcd4b8
Hannah Patricia Samson <hbsamson@up.edu.ph>
2026-09-02 17:51:05 +0800

    feat: add cors filter for preflight requests

---
+++ src/main/java/com/svi/tictactoe/utils/CorsFilter.java
@@ -0,0 +1,16 @@
+package com.svi.tictactoe.utils;
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+        response.getHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        response.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+    }
+}
