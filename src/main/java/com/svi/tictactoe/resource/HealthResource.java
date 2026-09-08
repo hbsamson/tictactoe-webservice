db8d7f95fef7d4b18a04446fb5e412975dbc5f70
Hannah Patricia Samson <hbsamson@up.edu.ph>
2026-09-08 16:53:44 +0800

    feat: add health check for server

---
+++ src/main/java/com/svi/tictactoe/resource/HealthResource.java
@@ -0,0 +1,16 @@
+package com.svi.tictactoe.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("health")
+public class HealthResource {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String check() {
+        return "Server is running";
+    }
+}
